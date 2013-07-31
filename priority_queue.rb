7e32bb724caeaaf6a0c579ba76adae3743e1a0a1
Andrew Djoga <andrew.djoga@gmail.com>
2013-07-31 15:14:10 +0300

    Max-oriented priority queue based on the binary heap

---
+++ priority_queue.rb
@@ -0,0 +1,45 @@
+# Max-oriented priority queue based on the binary heap
+class PriorityQueue
+  def initialize
+    @pq = [nil]
+  end
+
+  def insert(key)
+    @pq << key
+    swim(@pq.length - 1)
+  end
+
+  def delete_max
+    return nil     if length == 0
+    return @pq.pop if length == 1
+
+    max = @pq[1]
+    @pq[1] = @pq.pop
+    sink(1)
+    max
+  end
+
+  def length
+    @pq.length - 1
+  end
+
+  private
+
+  def swim(k)
+    while (k > 1) && (@pq[k/2] < @pq[k])
+      @pq[k/2], @pq[k] = @pq[k], @pq[k/2]
+      k /= 2
+    end
+  end
+
+  def sink(k)
+    while (2*k <= length)
+      j = 2*k
+      j += 1 if (j < length) && (@pq[j] < @pq[j + 1])
+      break if @pq[k] >= @pq[j]
+      @pq[k], @pq[j] = @pq[j], @pq[k]
+      k = j
+    end
+  end
+end
+
